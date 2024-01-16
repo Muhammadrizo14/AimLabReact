777457b3e711f8f35d859bf483a38e1ccaff7c43
Muhammadrizo <mtursunboev13@gmail.com>
2024-01-16 20:12:24 +0500

    fix errors with changing state

--- src/Components/Target/index.tsx
+++ src/Components/Target/index.tsx
@@ -1,18 +1,12 @@
-import styled from 'styled-components';
 import styles from './style.module.scss'
 import { useState } from 'react';
 
-
-interface IProp {
-    setTargetKilled: () => void
-}
-
-function index(props: IProp) {
-    const { setTargetKilled } = props
+function index(props: any) {
+    const { targetKilled } = props
     const [position, setPosition] = useState<string[]>([])
 
     const changePosition = () => {
-        setTargetKilled(prev => prev + 1)
+        targetKilled()
         const top = `${Math.floor(Math.random() * 90)}%`
         const left = `${Math.floor(Math.random() * (92 - 2) + 2)}%`
         setPosition([top, left])
