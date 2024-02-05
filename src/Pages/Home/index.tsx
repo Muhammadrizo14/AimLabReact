4f2cc494c3fb5bef1bc2acff0ba1e05f4d988429
Muhammadrizo <mtursunboev13@gmail.com>
2024-02-05 18:38:04 +0500

    vercelanalytics

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,23 +1,26 @@
 import styles from './style.module.scss'
 import Navbar from '../../Components/Navbar'
+import { Analytics } from '@vercel/analytics/react';
+
 
 function index() {
-    return (
-        <div className={styles.main}>
-            <Navbar />
-          <footer>
-            <a className={styles.github} href="https://github.com/Muhammadrizo14/AimLabReact">
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                   xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="m8 8-4 4 4 4m8 0 4-4-4-4m-2-3-4 14"/>
-              </svg>
-              <p>GitHub</p>
-            </a>
-            <p className={styles.author}>Created by <a href="https://t.me/ubuntuous">@ubuntuous</a></p>
-          </footer>
-        </div>
-    );
+  return (
+    <div className={styles.main}>
+      <Navbar />
+      <footer>
+        <a className={styles.github} href="https://github.com/Muhammadrizo14/AimLabReact">
+          <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="m8 8-4 4 4 4m8 0 4-4-4-4m-2-3-4 14" />
+          </svg>
+          <p>GitHub</p>
+        </a>
+        <p className={styles.author}>Created by <a href="https://t.me/ubuntuous">@ubuntuous</a></p>
+      </footer>
+      <Analytics />
+    </div>
+  );
 }
 
 export default index;
